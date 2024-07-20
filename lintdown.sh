7095b348ca95fdfe105ebffe9e1347bf948d68dd
ChillerDragon <ChillerDragon@gmail.com>
2024-07-20 09:52:18 +0800

    Add gen snippets helper

--- lintdown.sh
+++ lintdown.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-lint_go_snippets() {
+set -euo pipefail
+
+gen_snippets() {
 	local markdown_file="$1"
+	local markdown_lang="$2"
+	local lang_extension="${3:-$markdown_lang}"
 
 	mkdir -p tmp
-	awk '/^```go$/ {p=1}; p; /^```$/ {p=0;print"--- --- ---"}' "$markdown_file" |
-		grep -vE '^```(go)?$' |
+	awk '/^```'"$markdown_lang"'$/ {p=1}; p; /^```$/ {p=0;print"--- --- ---"}' "$markdown_file" |
+		grep -vE '^```('"$markdown_lang"')?$' |
 		csplit \
 		-z -s - '/--- --- ---/' \
 		'{*}' \
 		--suppress-matched \
-		-f tmp/readme_snippet_ -b '%02d.go'
+		-f tmp/readme_snippet_ -b '%02d.'"$lang_extension"
+}
+
+lint_go_snippets() {
+	local markdown_file="$1"
+	gen_snippets "$markdown_file" go
 
 	for snippet in ./tmp/readme_snippet_*.go; do
 		[ -f "$snippet" ] || continue
@@ -31,15 +40,7 @@ lint_go_snippets() {
 
 lint_lua_snippets() {
 	local markdown_file="$1"
-
-	mkdir -p tmp
-	awk '/^```lua$/ {p=1}; p; /^```$/ {p=0;print"--- --- ---"}' "$markdown_file" |
-		grep -vE '^```(lua)?$' |
-		csplit \
-		-z -s - '/--- --- ---/' \
-		'{*}' \
-		--suppress-matched \
-		-f tmp/readme_snippet_ -b '%02d.lua'
+	gen_snippets "$markdown_file" lua
 
 	for snippet in ./tmp/readme_snippet_*.lua; do
 		[ -f "$snippet" ] || continue
